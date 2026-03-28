cd59d0be19fcd92c7ce982b79f5b1493cb98cff0
Joe Schlesinger <157418651+joe-verkada@users.noreply.github.com>
2026-03-27 19:54:12 -0700

    datadog-cli: bump to 0.1.10

--- Formula/datadog-cli.rb
+++ Formula/datadog-cli.rb
@@ -1,28 +1,28 @@
 class DatadogCli < Formula
   desc "Read-only CLI for querying Datadog APIs"
   homepage "https://github.com/roboalchemist/datadog-cli"
-  version "0.1.8"
+  version "0.1.10"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.8/datadog-cli_0.1.8_darwin_amd64.tar.gz"
-      sha256 "0bdc326b57161f35f54312f8ad2b7c40990c7f32a435b75784670794e93734fc"
+      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.10/datadog-cli_0.1.10_darwin_amd64.tar.gz"
+      sha256 "7321b68217f8c8680057642571178fd76384f322dca66522fdd6081dfbbb4ea9"
     end
     on_arm do
-      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.8/datadog-cli_0.1.8_darwin_arm64.tar.gz"
-      sha256 "c9dbfb4ada00bbe83234bd19f4728c71c8e81e269a3317696dceeb18030708b6"
+      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.10/datadog-cli_0.1.10_darwin_arm64.tar.gz"
+      sha256 "596d9f4211856769a5ee24be3763fa1debb33fb203227dbd5b602dd9dfab4120"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.8/datadog-cli_0.1.8_linux_amd64.tar.gz"
-      sha256 "5ab23b1357db4bbb0c00510fe93b4ccfe0c975da9846c0b7bc4c9824bc82a353"
+      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.10/datadog-cli_0.1.10_linux_amd64.tar.gz"
+      sha256 "62827f444c5b9765c840d5b5e0560723e07434a084e7fd8616ce8b33213e723d"
     end
     on_arm do
-      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.8/datadog-cli_0.1.8_linux_arm64.tar.gz"
-      sha256 "2bdc914b2dd48acbfcfc07d58cef0fd01a5480747efdc5d744d4afd2b5488b2e"
+      url "https://github.com/roboalchemist/datadog-cli/releases/download/v0.1.10/datadog-cli_0.1.10_linux_arm64.tar.gz"
+      sha256 "263938605d13bd9e5aa4c44a6c8a8eef2b6389ed182576ec1e7746f0ccb8bb33"
     end
   end
 
