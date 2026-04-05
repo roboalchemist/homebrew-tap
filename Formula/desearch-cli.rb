68b1e402546042e52bc99b15c433f252f0d0188e
Joseph Schlesinger <joe@schlesinger.io>
2026-04-05 14:56:03 -0700

    desearch-cli: fix binary name to desearch-cli

--- Formula/desearch-cli.rb
+++ Formula/desearch-cli.rb
@@ -9,11 +9,11 @@ class DesearchCli < Formula
 
   def install
     ldflags = "-s -w -X github.com/roboalchemist/desearch-cli/cmd.version=#{version}"
-    system "go", "build", *std_go_args(ldflags:), "-o", bin/"desearch"
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"desearch-cli"
   end
 
   test do
-    assert_match "desearch", shell_output("#{bin}/desearch --help 2>&1")
-    assert_match version.to_s, shell_output("#{bin}/desearch version 2>&1")
+    assert_match "desearch-cli", shell_output("#{bin}/desearch-cli --help 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/desearch-cli version 2>&1")
   end
 end
