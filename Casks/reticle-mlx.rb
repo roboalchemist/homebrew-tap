e62ab6046518575eae7158ce792b76550a8ba532
roboalchemist <1588103+roboalchemist@users.noreply.github.com>
2026-07-27 20:25:03 -0700

    GH-13: bump Reticle MLX cask to 0.9.0

--- Casks/reticle-mlx.rb
+++ Casks/reticle-mlx.rb
@@ -1,6 +1,6 @@
 cask "reticle-mlx" do
-  version "0.8.1"
-  sha256 "528a84d7d11ced030460f35f7f507329198e0fc40f31a31777bac5d0084d6334"
+  version "0.9.0"
+  sha256 "cd7543e247e0aedb864f5d1f43a38cc0aa16fa75596eb4c021007c6a8b0eadcf"
 
   url "https://github.com/roboalchemist/reticle-mlx/releases/download/v#{version}/Reticle-MLX-#{version}.dmg"
   name "Reticle MLX"
